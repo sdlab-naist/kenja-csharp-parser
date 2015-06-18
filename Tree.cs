cb34fea39d55e5dc3fe1d1602f7ac2e0210db38e
Kenji Fujiwara <kenji-f@is.naist.jp>
2015-06-18 15:36:29 +0900

    added document comment

--- Tree.cs
+++ Tree.cs
@@ -3,18 +3,25 @@ using System.Text;
 
 namespace KenjaParser
 {
+    /// <summary>
+    /// A class which represents a Tree of Git.
+    /// This class provided interface to make tree file for kenja.
+    /// If you want to make a root tree, provide empty string to Name property.
+    /// </summary>
     public class Tree : GitObject
     {
         private const string START_TREE = "[TS] ";
         private const string END_TREE = "[TE] ";
         private List<GitObject> objects = new List<GitObject>();
         
+        /// <param name="name">
+        /// Name of a tree. For a root tree, empty string should be provided.
+        /// </param>
         public Tree(string name) : base(name){
         }
 
         public override void AppendToBuilder(StringBuilder builder)
         {
-            // TODO add document comment
             if(name != ""){
                 builder.AppendLine(START_TREE + name);
             }
